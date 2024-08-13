7b3f59891fe8bc0b92de2f3d76173418f33bd915
gon-sui <ayu.gun.4999@gmail.com>
2024-08-13 09:40:59 +0000

    Add project files

--- xdp-log/src/main.rs
+++ xdp-log/src/main.rs
@@ -1,12 +1,10 @@
-use aya::{include_bytes_aligned, Bpf};
+use aya::{include_bytes_aligned, Ebpf};
 use aya::maps::perf::AsyncPerfEventArray;
 use aya::programs::{Xdp, XdpFlags};
-use aya::util::online_cpus;
 use bytes::BytesMut;
 use clap::Parser;
 use log::info;
-use std::sync::Arc;
-use tokio::{signal, task};
+use tokio::signal;
 use xdp_log_common::PacketData;
 
 #[derive(Debug, Parser)]
@@ -18,71 +16,63 @@ struct Opt {
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     env_logger::init();
-
     let opt = Opt::parse();
 
+    // eBPFオブジェクトをロード
     #[cfg(debug_assertions)]
-    let bpf = Arc::new(Bpf::load(include_bytes_aligned!(
+    let mut bpf = Ebpf::load(include_bytes_aligned!(
         "../../target/bpfel-unknown-none/debug/xdp-log"
-    ))?);
+    ))?;
     #[cfg(not(debug_assertions))]
-    let bpf = Arc::new(Bpf::load(include_bytes_aligned!(
+    let mut bpf = Ebpf::load(include_bytes_aligned!(
         "../../target/bpfel-unknown-none/release/xdp-log"
-    ))?);
+    ))?;
 
-    // Set up and load the XDP program
-    let program = bpf.program("xdp_log").unwrap();
-    let mut xdp_program: &mut Xdp = program.try_into()?;
-    xdp_program.load()?;
-    xdp_program.attach(&opt.iface, XdpFlags::default())?;
+    // XDPプログラムをロードおよびアタッチ
+    let program: &mut Xdp = bpf.program_mut("xdp_log").unwrap().try_into()?;
+    program.load()?;
+    program.attach(&opt.iface, XdpFlags::default())?;
 
-    // Create the AsyncPerfEventArray
-    let map = bpf.map_mut("EVENTS").unwrap();
-    let perf_array = Arc::new(AsyncPerfEventArray::try_from(map)?);
+    // イベントマップを取得
+    let mut perf_array = AsyncPerfEventArray::try_from(bpf.map_mut("EVENTS").unwrap())?;
 
-    // Spawn async tasks for each CPU
-    for cpu_id in online_cpus()? {
-        let perf_array = Arc::clone(&perf_array);
+    // シングルコア用の処理
+    let cpu_id = 0;
+    let mut buf = perf_array.open(cpu_id, None)?;
 
-        task::spawn(async move {
-            let mut buf = perf_array.open(cpu_id, None).unwrap();
-            let mut buffers = (0..10)
-                .map(|_| BytesMut::with_capacity(1024))
-                .collect::<Vec<_>>();
+    // バッファを準備
+    let mut buffers = (0..10)
+        .map(|_| BytesMut::with_capacity(1024))
+        .collect::<Vec<_>>();
 
-            loop {
-                match buf.read_events(&mut buffers).await {
-                    Ok(events) => {
-                        for i in 0..events.read {
-                            let buf = &mut buffers[i];
-                            let ptr = buf.as_ptr() as *const PacketData;
-                            let data = unsafe { ptr.read_unaligned() };
+    info!("Listening for events...");
 
-                            let src_ip = data.src_addr.to_ne_bytes();
-                            let dst_ip = data.dst_addr.to_ne_bytes();
+    loop {
+        let events = buf.read_events(&mut buffers).await?;
 
-                            println!(
-                                "CPU: {} | {}.{}.{}.{}:{} -> {}.{}.{}.{}:{} | Size: {} bytes | Payload: {:?}",
-                                cpu_id,
-                                src_ip[0], src_ip[1], src_ip[2], src_ip[3], data.src_port,
-                                dst_ip[0], dst_ip[1], dst_ip[2], dst_ip[3], data.dst_port,
-                                data.size,
-                                &data.payload[..core::cmp::min(data.size as usize, data.payload.len())]
-                            );
-                        }
-                    }
-                    Err(e) => {
-                        eprintln!("Error reading events: {}", e);
-                    }
-                }
-            }
-        });
+        for i in 0..events.read {
+            let buf = &buffers[i];
+            let ptr = buf.as_ptr() as *const PacketData;
+            let data = unsafe { ptr.read_unaligned() };
+            
+            let src_ip = data.src_addr.to_ne_bytes();
+            let dst_ip = data.dst_addr.to_ne_bytes();
+            
+            println!(
+                "{}.{}.{}.{}:{} -> {}.{}.{}.{}:{} | Size: {} bytes | Payload: {:?}",
+                src_ip[0], src_ip[1], src_ip[2], src_ip[3], data.src_port,
+                dst_ip[0], dst_ip[1], dst_ip[2], dst_ip[3], data.dst_port,
+                data.size,
+                &data.payload[..core::cmp::min(data.size as usize, data.payload.len())]
+            );
+        }
+
+        // Ctrl+Cが押されたかチェック
+        if signal::ctrl_c().await.is_ok() {
+            info!("Exiting...");
+            break;
+        }
     }
-
-    info!("Waiting for Ctrl-C...");
-    signal::ctrl_c().await?;
-    info!("Exiting...");
 
     Ok(())
 }
-
